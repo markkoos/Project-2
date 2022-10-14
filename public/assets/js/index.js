37668cf0484ad6dc0acf320510a7228ff8047074
markkoos <markusimercado@gmail.com>
2022-10-13 20:05:37 -0700

    created Register/Login buttons for homepage and logic to make it function

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,14 @@
 // client side js 
+
+// Grabs the buttons from HTML page
+const registerBtn = document.getElementById(`register`);
+const loginBtn = document.getElementById(`login`);
+
+// Event Listeners for each button
+registerBtn.addEventListener(`click`, () => {
+    window.location.assign(`../form.html`)
+});
+
+loginBtn.addEventListener(`click`, () => {
+    window.location.assign(`../login.html`)
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./controllers');
+const sequelize = require(`./config/connection`);
 // import sequelize connection
 
 const app = express();
@@ -7,6 +8,16 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname, `/public/assets/index.html`))
+})
+
+app.get(`*`, (req, res) => {
+  res.sendFile(path.join(__dirname, `/public/assets/index.html`))
+})
 
 app.use(routes);
 
