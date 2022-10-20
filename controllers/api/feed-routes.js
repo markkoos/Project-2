09b9ee1739b1736e25df8f520ee6c4c2fdac1586
markkoos <markusimercado@gmail.com>
2022-10-19 19:08:42 -0700

    added featured gamer to feed page

--- controllers/api/feed-routes.js
+++ controllers/api/feed-routes.js
@@ -3,22 +3,6 @@ const { Form } = require(`../../models/form`);
 const { Feed } = require(`../../models/feed`);
 const axios = require(`axios`);
 
-router.post(`/`, async (req, res) => {
-  const name = req.body.username
-  try {
-      const userData = await Form.findOne({
-          where:{
-              username: name,
-          }
-      })
-      const user = userData.get({ plain: true});
-      res.render(`feed`, user);
-
-  } catch (err) {
-      res.status(500).json(err)
-  }
-});
-
 router.get(`/`, async (req, res) => {
   try {
   const options = {
@@ -58,33 +42,3 @@ router.get(`/`, async (req, res) => {
 
 
 
-
-// router.get(`/`, async (req, res) => {
-
-//     try {
-// 		res.render(`feed`);
-
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// });
-
-// axios.get(options)
-//   .then(function (response) {
-//     console.log(response);
-
-//     const options = {
-//     method: 'GET',
-//     url: 'https://video-game-news.p.rapidapi.com/all',
-//     headers: {
-//     'X-RapidAPI-Key': '570565d3dbmshaa50c044e67a7c3p147829jsn2a024d94fd11',
-//     'X-RapidAPI-Host': 'video-game-news.p.rapidapi.com'
-//   }
-// };
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   })
-//   .then(function (){
-
-//   })
