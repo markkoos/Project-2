6113ea534a1965602f66ba476e437f789aa3bed3
markkoos <markusimercado@gmail.com>
2022-10-19 18:05:13 -0700

    fixed API request

--- controllers/api/feed-routes.js
+++ controllers/api/feed-routes.js
@@ -1,6 +1,5 @@
 const router = require(`express`).Router();
 const { Form } = require(`../../models/form`);
-<<<<<<< HEAD
 const { Feed } = require(`../../models/feed`);
 const axios = require(`axios`);
 
@@ -17,20 +16,24 @@ router.get(`/`, async (req, res) => {
       }
   };
 
-  axios.request(options).then(function (response) {
-      console.log(response.data);
-      const resp = response.data;
+  axios.request(options).then(async function (response) {
+    //   console.log(response.data);
+      const resp = await response.data[1];
 
       const apiData = [];
 
-      apiData.push(response.data);
+      await apiData.push(resp);
 
-      res.render(`Feed`, apiData);
+      await console.log(apiData[0].text);
 
-      console.log(url)
+      await res.render(`feed`, apiData[0]);
+
+    //   console.log(url)
       }).catch(function (error) {
       console.error(error);
       });
+
+    // res.render(`feed`);
   } catch (err) {
       res.status(500).json(err)
   }
@@ -73,41 +76,4 @@ router.get(`/`, async (req, res) => {
 //   })
 
 
-=======
-
-// get route to the feed-page
-router.post(`/`, async (req, res) => {
-    const name = req.body.username
-    try {
-        const userData = await Form.findOne({
-            where:{
-                username: name,
-            }
-        })
-        const user = userData.get({ plain: true});
-        res.render(`feed`, user);
-
-    } catch (err) {
-        res.status(500).json(err)
-    }
-});
-
-
-// id create a check with username to pick
-
-router.get(`/:id`, async (req, res) => {
-    try {
-        const userData = await Form.findOne({ where: { id: req.params.id} });
-
-        const user = userData.get({ plain: true});
-
-        res.render(`feed`, user);
-
-    } catch (err) {
-        res.status(500).json(err)
-    }
-});
-
-module.exports = router;
->>>>>>> main
 
